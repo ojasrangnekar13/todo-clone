07f7bcf0b32a9176ec1ba4fa5062134b38271c58
Ojas Rangnekar <ojasrangnekar@gmail.com>
2021-10-10 20:28:26 +0530

    Added input, button and a two line list

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -4,6 +4,14 @@ const App = () => {
     return (
         <div className="App">
             <h1>sup</h1>
+
+            <input />
+            <button>Add TODO</button>
+
+            <ul>
+                <li>Go to bath</li>
+                <li>Go to code</li>
+            </ul>
         </div>
     )
 }
