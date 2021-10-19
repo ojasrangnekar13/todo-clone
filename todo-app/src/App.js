04dd25e1acf6ad1c15cd14e118c990039904fb96
Ojas Rangnekar <ojasrangnekar@gmail.com>
2021-10-19 11:26:03 +0530

    Added the AddTodo function

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -4,12 +4,16 @@ const App = () => {
     const [todos, setTodos] = useState(["Go to bath", "Go to code", "Go to see Naruto", "Go to study"])
     const [input, setInput] = useState("")
 
+    const addTodo = (event) => {
+        setTodos([...todos, input])
+    }
+
     return (
         <div className="App">
             <h1>TODO App</h1>
 
             <input value = {input} onChange={event => setInput(event.target.value)}/>
-            <button>Add TODO</button>
+            <button onClick={addTodo}>Add TODO</button>
 
             <ul>
                 {todos.map(todo => (
