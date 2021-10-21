85758ad1c13ada7ed673409a4d4e6cefd8eb3b51
OSR <81454124+ojasrangnekar13@users.noreply.github.com>
2021-10-21 23:28:03 +0530

    Update firebase.js

--- todo-app/src/firebase.js
+++ todo-app/src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initialiseApp({
-    apiKey: "AIzaSyCg1yNxN2fJwURmEhhYhX9PwgMr8jdp2lY",
+    apiKey: "apikey",
     authDomain: "todo-app-664c7.firebaseapp.com",
     projectId: "todo-app-664c7",
     storageBucket: "todo-app-664c7.appspot.com",
-    messagingSenderId: "861448113760",
-    appId: "1:861448113760:web:2f178eca90ae10ebda7d05",
-    measurementId: "G-6RRQL18G6Q"
+    messagingSenderId: "senderid",
+    appId: "appid",
+    measurementId: "msrid"
 })
 
 const db = firebaseApp.firestore()
